e7fd27630335ee2a34b6a882e981ea602fa1ab7c
Milad Dehghan <milad.dehghan@hotmail.com>
2024-01-06 23:29:29 +0330

    add config reader

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	LogLevel string
+	Version  string
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config")
+	viper.AutomaticEnv()
+
+	var config Config
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+
+	return &config, nil
+}
